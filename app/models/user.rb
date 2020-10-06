a8d2b2e60df6b2bacb755f267d9a18b57cb5465b
kazusystemakure <70381817+kazusystemakure@users.noreply.github.com>
2020-10-06 22:03:48 +0900

    バリデーション、エラー表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters.'}
+    validates :namekana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters.' }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
+    validates :prefecture
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
+    validates :hobby
+    validates :birth_date
+  end
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,10}\z/i, message: 'Include both letters and numbers.' }
 end
