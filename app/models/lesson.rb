23687d852114744730b690b962c8b0d87a3752e3
kazusystemakure <70381817+kazusystemakure@users.noreply.github.com>
2020-10-07 16:57:21 +0900

    モデル単体テスト実施

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,13 @@
 class Lesson < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :text
+    validates :price, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }
+  end
+
+  validates_inclusion_of :price, in: 500..50_000, message: 'Out of setting range'
 end
