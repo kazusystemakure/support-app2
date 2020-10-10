f63666c71b91c7c0132c2ab5c0c1782c6608cefd
kazusystemakure <70381817+kazusystemakure@users.noreply.github.com>
2020-10-10 16:12:51 +0900

    マイページ修正(show),会員修正遷移

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @lessons = @user.lessons
+    @orders = @user.orders
   end
 
   def edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :lessons
+  has_many :orders
   has_one_attached :image
 
   with_options presence: true do
