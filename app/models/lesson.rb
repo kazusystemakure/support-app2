a87b36612d4f06fba7289bcd52feb51a5db0b0cd
kazusystemakure <70381817+kazusystemakure@users.noreply.github.com>
2020-10-11 21:51:38 +0900

    Lessonカラム変更に伴う修正（ActivHash追加）

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,13 +2,16 @@ class Lesson < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :level
+
   with_options presence: true do
     validates :image
-    validates :content
     validates :name
     validates :text
     validates :price, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }
   end
 
+  validates :level_id, numericality: { other_than: 1, message: 'Select' }
   validates_inclusion_of :price, in: 500..50_000, message: 'Out of setting range'
 end
---
+++ app/models/level.rb
@@ -0,0 +1,9 @@
+class Level < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '多め（しっかり汗をかく）' },
+    { id: 3, name: '適度（適度に汗をかく）' },
+    { id: 4, name: '軽め（少々汗をかく)' },
+    { id: 5, name: 'その他（身体を動かさない)' },
+  ]
+end
