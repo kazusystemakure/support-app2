37ec33a97539b3136fde23ff273b7e687935fe85
kazusystemakure <70381817+kazusystemakure@users.noreply.github.com>
2020-10-12 09:35:04 +0900

    未ログイン・別ユーザーのページ遷移リダイレクト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :move_to_new_user_session
+  before_action :move_to_index
+
   def show
     @user = User.find(params[:id])
     @lessons = @user.lessons
@@ -22,5 +25,16 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :namekana, :introduction, :email, :password ,:password_confirmation, :postal_code, :prefecture, :city, :address, :phone_number, :skill, :birth_date, :birth_date, :image)
   end
+  
+  def move_to_new_user_session
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
 
+  def move_to_index
+    @user = User.find(params[:id])
+    return redirect_to root_path if current_user.id != @user.id
+  end
+    
 end
